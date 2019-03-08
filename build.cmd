a373aef4e0b463ff1ca6b597cd8df128953b5234
Atif Aziz <code@raboof.com>
2019-03-08 16:27:27 +0100

    Remove "dotnet --info" in build script

--- build.cmd
+++ build.cmd
@@ -6,7 +6,6 @@ goto :EOF
 
 :main
     dotnet restore ^
- && dotnet --info ^
  && dotnet run --no-restore -c Release -p src\RdatasetsGenerator\RdatasetsGenerator.csproj -- data src\Rdatasets ^
  && dotnet build --no-restore -c Debug ^
  && dotnet build --no-restore -c Release
--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")"
-dotnet --info
 dotnet restore
 dotnet run --no-restore -c Release -p src/RdatasetsGenerator/RdatasetsGenerator.csproj -- data src/Rdatasets
 for c in Debug Release; do
