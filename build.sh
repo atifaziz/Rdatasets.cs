8293792a3384c93e0d1cf31f1e339c9cdb3531d6
Atif Aziz <code@raboof.com>
2019-03-08 16:24:41 +0100

    Improve working dir handling in bash scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-[[ -e build.sh ]] || { echo >&2 "Please cd into the script location before running it."; exit 1; }
 set -e
+cd "$(dirname "$0")"
 dotnet --info
 dotnet restore
 dotnet run --no-restore -c Release -p src/RdatasetsGenerator/RdatasetsGenerator.csproj -- data src/Rdatasets
